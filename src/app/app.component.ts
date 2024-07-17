645680e9e58dbb4073b568da5509e19c061a103c
Tafadzwa <tafadzwa.dube@dev200.co.za>
2024-07-17 13:40:02 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TodolistComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-app';
---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.css',
+})
+export class TodolistComponent implements OnInit {
+  taskArray = [
+    {
+      taskName: 'Brush teeth',
+      isCompleted: false,
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+    });
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+}
